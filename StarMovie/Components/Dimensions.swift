75d1add893e1f7993624d52e254b701eb02e9f21
litunny <liquidcoding2009@gmail.com>
2022-01-23 18:08:42 +0100

    Done with first segment control

--- StarMovie/Components/Dimensions.swift
+++ StarMovie/Components/Dimensions.swift
@@ -12,4 +12,16 @@ enum Dimensions {
     //Logo
     static let splashScreenLogoWidth: CGFloat = 108
     static let splashScreenLogoHeight: CGFloat = 184.03
+
+    //Segment
+    static let segmentHeight: CGFloat = 32.0
+    static let segmentWidth: CGFloat = 166.0
+
+    //CGFloat Positive Values
+    static let two: CGFloat = 2
+    static let four: CGFloat = 4
+    static let fourteen: CGFloat = 14
+    static let thirteen: CGFloat = 13
+    static let sixteen: CGFloat = 16
+    static let twenty: CGFloat = 20
 }
--- StarMovie/Components/Segment/StarSegmentControl.swift
+++ StarMovie/Components/Segment/StarSegmentControl.swift
@@ -15,12 +15,29 @@ class StartSegmentControl: UIView {
             firstLabelView.text = newValue
         }
     }
+
     private var secondLabel: String
 
     var firstLabelView: UILabel = {
         let label = UILabel()
-
+        label.text = "Now showing"
+        label.textColor = StarColors.whiteColor
+        label.font = .boldSystemFont(ofSize: Dimensions.fourteen)
         return label
+    }()
+
+    var firstSegment: UIView = {
+        let view = UIView()
+        view.backgroundColor = StarColors.redColor
+        view.layer.cornerRadius = Dimensions.sixteen
+        return view
+    }()
+
+    var secondSegment: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = Dimensions.sixteen
+        return view
     }()
 
 
@@ -30,15 +47,56 @@ class StartSegmentControl: UIView {
 
         super.init(frame: .zero)
 
-        self.layer.borderColor = UIColor.systemGray2.cgColor
-        self.layer.borderWidth = 2
-        self.layer.cornerRadius = 20
-        self.backgroundColor = .white
-        
-        NSLayoutConstraint.activate([
-            
-        ])
+        //Setup View
+        self.setupView()
 
+        //Setup Constraints
+        self.setupConstraints()
+
+        //Setup Selectors
+        self.setupSelectors()
+    }
+
+    func setupView() {
+        //Prepare view
+        self.prepareView()
+
+        //Inject subviews
+        self.addSubview([firstSegment, secondSegment])
+
+        self.firstSegment.addSubview([
+            firstLabelView
+        ])
+    }
+
+    func prepareView() {
+        self.layer.borderColor = UIColor.systemGray2.cgColor
+        self.layer.borderWidth = Dimensions.two
+        self.layer.cornerRadius = Dimensions.twenty
+        self.backgroundColor = .white
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+
+            //First Segment Constraints
+            firstSegment.heightAnchor.constraint(equalToConstant: Dimensions.segmentHeight),
+            firstSegment.widthAnchor.constraint(equalToConstant: Dimensions.segmentWidth),
+            firstSegment.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Dimensions.four),
+            firstSegment.topAnchor.constraint(equalTo: self.topAnchor, constant: Dimensions.four),
+
+            //Second Segment Constraints
+            secondSegment.heightAnchor.constraint(equalToConstant: Dimensions.segmentHeight),
+            secondSegment.widthAnchor.constraint(equalToConstant: Dimensions.segmentWidth),
+            secondSegment.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Dimensions.four),
+            secondSegment.topAnchor.constraint(equalTo: self.topAnchor, constant: Dimensions.four),
+
+            firstLabelView.centerYAnchor.constraint(equalTo: firstSegment.centerYAnchor),
+            firstLabelView.centerXAnchor.constraint(equalTo: firstSegment.centerXAnchor),
+        ])
+    }
+
+    func setupSelectors() {
         self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.handleTapped)))
     }
 
